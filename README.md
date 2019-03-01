0a852a136bc7029474856d3cb5d5ec5c902a6c6a
taotao.lin <cnredink@gmail.com>
2019-03-01 17:33:17 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Extrace
+
+Extrace is an Elixir wrapper for [Recon Trace](https://ferd.github.io/recon/recon_trace.html).
+
+Original library [repository](https://github.com/tatsuya6502/recon_ex)
---
+++ mix.exs
@@ -0,0 +1,33 @@
+defmodule Extrace.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :extrace,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      description: "Elixir wrapper for Recon Trace.",
+      package: [
+        name: "extrace",
+        maintainers: ["redink"],
+        licenses: ["Apache 2.0"],
+        links: %{"GitHub" => "https://github.com/redink/extrace"}
+      ]
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:recon, "~> 2.4"},
+      {:ex_doc, "~> 0.19", only: [:dev, :test]}
+    ]
+  end
+end
