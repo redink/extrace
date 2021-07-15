51f03c80ba35993f76ab34aff05ef88d1f47e8bd
Kian-Meng, Ang <kianmeng@cpan.org>
2021-07-15 20:33:09 +0800

    Misc doc changes
    Besides other documentation changes, this commit ensures the generated
    HTML doc for HexDocs.pm will become the source of truth for this Elixir
    library and leverage on latest features of ExDoc.

--- mix.exs
+++ mix.exs
@@ -1,20 +1,18 @@
 defmodule Extrace.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/redink/extrace"
+  @version "0.3.0"
+
   def project do
     [
       app: :extrace,
-      version: "0.3.0",
+      version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps(),
-      description: "Elixir wrapper for Recon Trace.",
-      package: [
-        name: "extrace",
-        maintainers: ["redink"],
-        licenses: ["Apache 2.0"],
-        links: %{"GitHub" => "https://github.com/redink/extrace"}
-      ]
+      docs: docs()
     ]
   end
 
@@ -27,7 +25,32 @@ defmodule Extrace.MixProject do
   defp deps do
     [
       {:recon, "~> 2.5"},
-      {:ex_doc, "~> 0.19", only: [:dev, :test]}
+      {:ex_doc, ">= 0.0.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      name: "extrace",
+      description: "Elixir wrapper for Recon Trace.",
+      maintainers: ["redink"],
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => @source_url
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "ORIGINAL-LICENSE.md": [title: "Original License"],
+        LICENSE: [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      formatters: ["html"]
     ]
   end
 end
