6fdf4d9eb7c57513cf7e0037539ee03b215a6705
Shou Ya <shouya@users.noreply.github.com>
2023-01-05 20:36:47 +0900

    bump version and required elixir version

--- mix.exs
+++ mix.exs
@@ -2,13 +2,13 @@ defmodule Extrace.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/redink/extrace"
-  @version "0.3.1"
+  @version "0.4.0"
 
   def project do
     [
       app: :extrace,
       version: @version,
-      elixir: "~> 1.7",
+      elixir: "~> 1.14.0",
       start_permanent: Mix.env() == :prod,
       package: package(),
       deps: deps(),
